2b9f0c48cf815d61f7e57aba9eeade901e99067a
Joe Hellerstein <jmh@berkeley.edu>
2025-05-20 22:53:00 -0700

    images and about

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,28 +10,20 @@ const Layout = ({ children }) => {
     <>
       <div className="banner-hero">
         <Link to="/">
-        <img
-          src="img/async-banner.svg"
-          alt="Async Stream Banner"
-          className="site-banner"
-        />
+          <img
+            src="/img/async-banner.svg" // Fixed path
+            alt="Async Stream Banner"
+            className="site-banner"
+          />
         </Link>
         <div className="banner-overlay">
           <div className="banner-header">
-            {/* <div className="site-title">
-              <Link to="/">Async Stream</Link>
-            </div> */}
             <nav className="site-nav">
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
               <Link to="/contact">Contact</Link>
             </nav>
           </div>
-          {/* {isHome && (
-            <div className="banner-tagline">
-              Occasional thoughts on coding, computing and data.
-            </div>
-          )} */}
         </div>
       </div>
       <div className="container">
--- src/pages/about.js
+++ src/pages/about.js
@@ -9,22 +9,25 @@ const AboutPage = () => {
       <div className="content">
         <h1>About</h1>
         <p>
-          Hi! I'm Joe Hellerstein, a computer scientist at UC Berkeley. I
-          study data-centric systems and the way they drive computing.
+          I work as a computer scientist at UC Berkeley. I
+          focus on data-centric systems and their central role in computing.
           I came up as a database systems researcher, but 
           in recent years I've been particularly focused on the design of 
-          programming languages for distributed systems (e.g <a href="https://hydro.run">Hydro</a>).
+          programming languages for distributed systems (e.g <a href="https://hydro.run">Hydro</a>),
+          which can benefit enormously from a data-centric lens.
         </p>
         <p>
             I like to ground my work in real-world efforts, including startups I've helped lead
             like <a href="https://runllm.com">RunLLM</a>, <a href="https://en.wikipedia.org/wiki/Trifacta">Trifacta</a>,
             and <a href="https://en.wikipedia.org/wiki/Greenplum">Greenplum</a>, 
-            as well as interaction with established companies.
+            as well as established companies I've worked with over the years.
         </p>
         <p>
-            I'm also a humble lifelong student of the trumpet. To quote Dizzy Gillespie, 
-            "Some days you get up and put the horn to your chops and it sounds pretty good and you win. 
-            Some days you try and nothing works and the horn wins. This goes on and on and then you die and the horn wins."
+            I'm also a lifelong student of the trumpet. To quote Dizzy Gillespie, 
+            <blockquote>Some days you get up and put the horn to your chops and it sounds pretty good and you win. 
+            Some days you try and nothing works and the horn wins. This goes on and on and then you die and the horn wins.
+            </blockquote>
+            Humility and a lack of control, baked into a daily practice. Highly recommended!
         </p>
       </div>
     </Layout>
